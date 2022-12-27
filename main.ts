21b907efea58d97f6f69e830659a0380837e0950
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-27 18:54:05 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    leva_led.showRainbow(1, 360)
 })
+input.onButtonPressed(Button.B, function () {
+    prava_led.showBarGraph(2, 2)
+})
+let prava_led: neopixel.Strip = null
+let leva_led: neopixel.Strip = null
+let pasek_led = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
+leva_led = pasek_led.range(0, 1)
+prava_led = pasek_led.range(1, 1)
